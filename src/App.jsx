94773756031e3f53b92bf65d30ffd536c2b7943e
kyawkoko232 <kyawkoko9m@gmail.com>
2023-07-03 19:53:57 +0630

    main

--- src/App.jsx
+++ src/App.jsx
@@ -3,10 +3,7 @@ import ProfileCard from "./ProfileCard";
 function App(){
   return(
     <div>
-      <div>Digital Assitant</div>
-      <ProfileCard name="Kyaw Ko Ko" title="Alexa" handle="@alexa" />
-      <ProfileCard name="object destruct" title="siri" handle="siri" />
-      <ProfileCard name="array destruct" title="chatgpt" handle="chatgpt" />
+    
 
     </div>
   )
