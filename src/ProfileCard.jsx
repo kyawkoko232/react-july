c37ad5e1ccacd9834a9ba8c613e0c81d44929196
kyawkoko232 <kyawkoko9m@gmail.com>
2023-07-03 19:52:11 +0630

    not new

---
+++ src/ProfileCard.jsx
@@ -0,0 +1,15 @@
+function ProfileCard({name, title, handle}){
+    // const title = props.title;
+    // const handle = props.handle
+   
+    return (
+        <div>
+            <div>title is {title} </div>
+            <div>name is {name} </div>
+
+            <div>Handle is {handle} </div>
+        </div>
+    )
+}
+
+export default ProfileCard
