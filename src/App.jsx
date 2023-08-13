275b1d2f920c1b84d4bad76dd75e3fc7c61190a1
kyawkoko232 <kyawkoko9m@gmail.com>
2023-08-13 16:47:45 +0630

    basic setter and state

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,24 @@
+import { useState } from "react";
+
 function App() {
 
+  let [count, setCount] = useState (0); 
+  const handleClick = ()=> {
+    setCount(count + 1);
+  }
+
+  const resetClick = ()=> {
+    setCount(count = 0);
+  }
 
   return <div>
-    <button onClick={
-       () => {
-        console.log('Button was Clicked');
-      }
-    }>
+    <button onClick={handleClick}>
       Add Animal
     </button>
+    <button onClick={resetClick}>
+      Reset Click
+    </button>
+    <div>Number of Animal: {count} </div>
   </div>
 }
 export default App;
