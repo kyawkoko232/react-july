4e41ad93b0e30053ba1dbab2a94ae2cdf10c1c01
kyawkoko232 <kyawkoko9m@gmail.com>
2023-08-13 22:45:52 +0630

    map loop ok

--- src/App.jsx
+++ src/App.jsx
@@ -1,24 +1,31 @@
 import { useState } from "react";
+import AnimalShow from "./AnimalShow";
+
+
+function getRandomAnimal() {
+  const animals = ['bird', 'cat', 'cow', 'dog', 'gator', 'horse'];
+  return animals[Math.floor(Math.random() * animals.length)];
+}
 
 function App() {
 
-  let [count, setCount] = useState (0); 
-  const handleClick = ()=> {
-    setCount(count + 1);
-  }
+  const [animals, setAnimals] = useState([]);
 
-  const resetClick = ()=> {
-    setCount(count = 0);
-  }
+  const handleClick = () => {
+    setAnimals([...animals, getRandomAnimal()]);
+  };
 
-  return <div>
-    <button onClick={handleClick}>
-      Add Animal
-    </button>
-    <button onClick={resetClick}>
-      Reset Click
-    </button>
-    <div>Number of Animal: {count} </div>
-  </div>
+  const renderedAnimals = animals.map((animal, index) => {
+
+    return <AnimalShow type={animal} key={index} />;
+  })
+
+
+  return (
+    <div>
+      <button onClick={handleClick}> Click Me </button>
+      <div>{renderedAnimals}</div>
+    </div>
+  )
 }
 export default App;
