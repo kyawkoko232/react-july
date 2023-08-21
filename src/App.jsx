49dbdf5f5a1ce88556e4dbbca19b8c7bc49ea0a7
Kyaw Ko Ko <kyawkoko9m@gmail.com>
2023-08-21 22:34:12 +0630

    tailwind installed

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,12 @@
-import ProfileCard from "./ProfileCard";
+
 
 function App(){
   return(
     <div>
     
+    <h1 className="text-3xl  font-bold underline">
+      Hello world!
+    </h1>
 
     </div>
   )
--- src/main.jsx
+++ src/main.jsx
@@ -1,6 +1,8 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import App from './App.jsx'
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import './style.css';
+import App from './App.jsx';
+
 
 
 ReactDOM.createRoot(document.getElementById('root')).render(
