1084fbdfd0f140f2c877b40be7062b7c49811c12
kyawkoko232 <kyawkoko9m@gmail.com>
2023-08-24 22:01:48 +0630

    updated userShow with nested json

--- src/components/UserShow.jsx
+++ src/components/UserShow.jsx
@@ -6,10 +6,14 @@ function UserShow({ user }) {
       <ul className="list-disc flex flex-col gap-y-2 bg-slate-900 text-gray-400 shadow-xl font-roboto hover:bg-blue-500 hover:text-gray-200 hover:rounded-2xl transition-all duration-500 border px-10 py-5 rounded-lg">
         <li>Name - {user.name}</li>
         <li>Email - {user.email}</li>
-        <li>Email - {user.email}</li>
-        <li>Verify At - {user.email_verified_at}</li>
+        <li>Verfied at - {user.email_verified}</li>
+        <li>Gender - {user.gender}</li>
+  
         <li>Role - {user.role}</li>
-        <li>Joined - {user.created_at}</li>
+        <li>
+            Zodiac Sign - {user.zodiac.sun_sign}
+        </li>
+       
       </ul>
       
     </div>
