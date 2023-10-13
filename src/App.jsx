69b2b080a3830146678c099ecea0fcc998409f56
kyawkoko232 <kyawkoko9m@gmail.com>
2023-10-13 13:10:16 +0630

    108

--- src/App.jsx
+++ src/App.jsx
@@ -8,15 +8,20 @@ function App(){
 
   const [books, setBooks] = useState([]);
 
-  const createBook = (title) => {
-    console.log('Need to add Book with titile', title);
-  }
-  return(
-    <div>
-    
-    <BookCreate  onCreate={createBook} />
-    </div>
-  )
+ const createBook = (title) => {
+  const updatedBooks = [
+    ...books, 
+    { id: Math.round(Math.random() * 9999), title}
+  ]
+  setBooks(updatedBooks);
+ }
+ return(
+  <div>
+  {books.length}
+  <BookCreate  onCreate={createBook} />
+  </div>
+)
+  
 }
 
 export default App;
