78a289ded2803a1958c1cf511d7a4ff9e07852cd
kyawkoko232 <kyawkoko9m@gmail.com>
2023-10-16 17:12:49 +0630

    text change

--- src/components/BookShow.jsx
+++ src/components/BookShow.jsx
@@ -1,21 +1,36 @@
+import { useState } from "react";
+import BookEdit from './BookEdit';
+
 function BookShow( {book, onDelete}){
-    const handleClick = ()=> {
-        onDelete(book.id);
-        console.log("Delete Clicked");
-        console.log(book.id)
+  const [showEdit, setShowEdit] = useState(false);
+    const handleDeleteClick = ()=> {
+        onDelete(content);
     };
+
+    const handleEditClick = ()=> {
+      setShowEdit(!showEdit);
+    }
+
+    let content = <h3>{book.title}</h3>;
+    if (showEdit) {
+      content = <BookEdit />;
+    }
+
     return (
-    <div className=" transition-all duration-1000">
+    <div className="">
      
       <div className="bg-slate-700 relative  max-w-sm flex-1 hover:bg-gray-300 transition-all duration-300 hover:text-black-500 p-5 m-2 rounded-xl text-slate-300">
       <div className="actions">
+        <button className="edit rounded-lg px-2 text-white hover:text-slate-600 hover:bg-green-300  bg-black-500 " onClick={handleEditClick}> 
+          Edit
+        </button>
         <button 
-        className="w-8 h-8 leading-none absolute bg-red-600 right-1 top-1 rounded-full" 
-       onClick={handleClick}
+        className="w-8 h-8 hover:bg-blue-600 leading-none absolute bg-red-600 right-1 top-1 rounded-full" 
+       onClick={handleDeleteClick}
         > x </button>
       </div>
       <div className="text-sm">id - {book.id}</div>
-      <div className="text-xl">book titile - {book.title}</div>
+      <div className="text-xl">book titile - {content}</div>
       </div>
     </div>
     )
