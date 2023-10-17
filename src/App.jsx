0580d0b8f36f417c69220180f354a58d16896765
kyawkoko232 <kyawkoko9m@gmail.com>
2023-10-17 19:31:32 +0630

    api finished

--- src/App.jsx
+++ src/App.jsx
@@ -18,19 +18,26 @@ function App(){
   }, []);
 
 
-  const editBookById = (id, newTitle) => {
+  const editBookById = async (id, newTitle) => {
+    const response = await axios.put(`http://localhost:3001/books/${id}`,{
+      title : newTitle
+    });
+     console.log(response); 
+
     const updatedBooks = books.map((book) => {
       if (book.id === id) {
-        return { ...book, title: newTitle };
+        return { ...book, ...response.data };
       }
 
-      return book;
+      return book; 
     });
 
     setBooks(updatedBooks);
   };
 
-  const deleteBookById = (id) => {
+  const deleteBookById = async (id) => {
+    await axios.delete(`http://localhost:3001/books/${id}`)
+
     const updatedBooks = books.filter((book) => {
       return book.id !== id;
     });
