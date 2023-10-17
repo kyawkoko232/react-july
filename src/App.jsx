61e561c89d413c384021d802bf2148eb4e71b913
kyawkoko232 <kyawkoko9m@gmail.com>
2023-10-17 17:39:38 +0630

    use effect

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,21 @@
-import { useState } from "react";
+import { useState, useEffect } from "react";
 import BookCreate from './components/BookCreate';
 import BookList from './components/BookList';
-
+import axios from "axios";
 
 
 function App(){
 
   const [books, setBooks] = useState([]);
+
+  const fetchBooks = async() => {
+    const response = await axios.get('http://localhost:3001/books');
+    setBooks(response.data);
+  };
+
+  useEffect(()=>{
+    fetchBooks();
+  }, []);
 
 
   const editBookById = (id, newTitle) => {
@@ -28,12 +37,15 @@ function App(){
     setBooks(updatedBooks);
   };
   
- const createBook = (title) => {
+ const createBook =  async(title) => {
+  const response = await axios.post('http://localhost:3001/books',{
+    title
+  });
+  
+  console.log(response)
   const updatedBooks = [
     ...books, 
-    { id: Math.round(Math.random() * 9999),
-       title
-    },
+   response.data
   ]
   setBooks(updatedBooks);
  }
