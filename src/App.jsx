e6ba2e7d4ba0e6636188d5c1372c4daa41472894
kyawkoko232 <kyawkoko9m@gmail.com>
2023-10-25 23:27:52 +0630

    button

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,40 @@
+import Button from './Button';
+import { GoBell } from 'react-icons/go'
 
+function App() {
 
-function App(){
-  return(
-    <div  data-theme="cupcake" className="h-screen">
-    
-    <h1 className="text-3xl">
-      Hello world!
-    </h1>
+  const handleClick = ()=> {
+    console.log('Click');
+  }
 
+  return (
+    <div className='flex flex-wrap justify-center items-center gap-5 p-5'>
+      <div className=''>
+        <Button onClick={handleClick}  primary rounded>
+         <GoBell /> Click me!!
+        </Button>
+      </div>
+      <div>
+        <Button onClick={handleClick} danger outline>
+        
+          Buy Now!
+        </Button>
+      </div>
+      <div>
+        <Button onClick={handleClick} warning>See Deal!</Button>
+      </div>
+      <div>
+        <Button onClick={handleClick} secondary outline>
+          Hide Ads!
+        </Button>
+      </div>
+      <div>
+        <Button onClick={handleClick} secondary rounded>
+          Something!
+        </Button>
+      </div>
     </div>
-  )
+  );
 }
 
 export default App;
---
+++ src/Button.jsx
@@ -0,0 +1,58 @@
+import className from "classnames";
+
+const finalClassName = className({
+    'btn-primary' : true,
+    'text-yellow-500' : true,
+
+    
+})
+console.log(finalClassName)
+
+function Button({
+  children,
+  primary,
+  secondary,
+  success,
+  warning,
+  danger,
+  outline,
+  rounded,
+  ...rest
+}) {
+    console.log(rest)
+    const classes = className('btn btn-sm', {
+        'btn-primary' : primary,
+        'btn-secondary': secondary,
+        'btn-success' : success,
+        'btn-warning' : warning,
+        'btn-danger' : danger,
+        'btn-outline' : outline,
+        'btn-rounded' : rounded,
+        'text-blue-500' : outline && primary,
+        'text-gray-500' : outline && secondary,
+        'text-green-500' : outline && success,
+        'text-yellow-400' : outline && warning,
+        'text-red-500' : outline && danger,
+        
+    })
+  return <button  className={classes}>{children}</button>;
+}
+
+Button.propTypes = {
+  checkVariationValue: ({ primary, secondary, success, warning, danger }) => {
+    const count =
+      Number(!!primary) +
+      Number(!!secondary) +
+      Number(!!warning) +
+      Number(!!success) +
+      Number(!!danger);
+
+    if (count > 1) {
+      return new Error(
+        'Only one of primary, secondary, success, warning, danger can be true'
+      );
+    }
+  },
+};
+
+export default Button;
